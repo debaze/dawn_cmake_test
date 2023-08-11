a88c937939da05885e66c927d7dd8375fb2f8d31
matteokeole <matteo@keole.net>
2023-08-11 18:28:06 +0200

    feat: GLFW window tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "GLFW/glfw3.h"
+
+const uint32_t WINDOW_WIDTH	= 512;
+const uint32_t WINDOW_HEIGHT = 256;
+const char* WINDOW_TITLE = "Title";
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	if (!glfwInit()) return 1;
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+	}
 }
