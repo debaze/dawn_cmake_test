5a0b42b620d57af90b8edbc819606ddcecd04563
matteokeole <matteo@keole.net>
2023-08-12 11:37:16 +0200

    feat: Add readFile()

--- src/utils.cpp
+++ src/utils.cpp
@@ -7,3 +7,21 @@ void print(const char* message) {
 void print(bool condition, const char* message) {
 	std::cout << message << ": " << (condition ? "OK" : "KO") << std::endl;
 }
+
+const char* readFile(const char* path) {
+	const char* result;
+	std::ifstream file(path);
+
+	if (file.is_open()) {
+		std::string content(
+			(std::istreambuf_iterator<char>(file)),
+			(std::istreambuf_iterator<char>())
+		);
+
+		result = content.c_str();
+	}
+
+	file.close();
+
+	return result;
+}
--- src/utils.hpp
+++ src/utils.hpp
@@ -2,3 +2,5 @@
 
 void print(const char* message);
 void print(bool condition, const char* message);
+
+const char* readFile(const char* path);
